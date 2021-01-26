f526d10aace03b9f66eea7b13a9cf2ca5d1156b8
Petter Fagerlund <PFAGERLU@volvocars.com>
2021-01-27 00:21:35 +0100

    resets value

--- cypress/integration/frontEndFlowTest.js
+++ cypress/integration/frontEndFlowTest.js
@@ -1,17 +1,21 @@
 describe('Base', function () {
     it('should load home page', function () {
-    cy.visit('https://localhost:49163/bugs')
-
+        cy.visit('https://localhost:49163/bugs')
     })
     it('Page should contain crud operations', () => {
         cy.get('#bugsCrudOperations');
     })
-    it('Should reload the page upon press of crud buttons', () => {
-        cy.get("#bugName").type("no product")
+    it('Should reload the page upon press of crud buttons, and reset to inital value', () => {
+        var initalBug = cy.get("#InitialbugName");
+        cy.get("#bugName").type("no product");
         cy.get('#update').click();
         cy.url().should('eq', 'https://localhost:49163/bugs');
+        cy.get("#bugName").type(initalBug);
+        cy.get('#update').click();
+        cy.url().should('eq', 'https://localhost:49163/bugs');
+        initalBug.should('not.have.value', 'No Product')
     })
-    it('Should  reset the state of the webapp', () => {
+    it('Should reset the state of the webapp', () => {
         cy.visit('https://localhost:49163/bugs');
         cy.url().should('eq', 'https://localhost:49163/bugs');
     })
